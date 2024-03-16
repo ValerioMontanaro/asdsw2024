d7bf373270020fc478c7fa73d7302b35040487f9
ValerioMontanaro <valerio.montanaro@alcampus.it>
2024-03-16 16:10:17 +0000

    Lezione tcp

--- socket/tcp_echo_server.py
+++ socket/tcp_echo_server.py
@@ -2,33 +2,48 @@ import socket
 from sys import argv
 import time 
 
+# i socket servono in generale per far comunicare macchine diverse quindi applicazioni diverse
+
 localIP     = argv[1]
 localPORT   = int(argv[2])
 
+# Solo per definire  il tipo di socket
 TCPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+
+# operazione di bind per associare il socket ad un indirizzo IP e una porta
 TCPServerSocket.bind((localIP, localPORT))
 
 print('TCP Server UP ({},{}), waiting for connections ...'.format(localIP, localPORT))
+
+# mi metto in ascolto
 TCPServerSocket.listen()
 
 # Accetto una nuova connessione
+# conn è il socket che mi serve per comunicare con il client
+# addr punta a una tupla che contiene l'indirizzo del client e la porta del client 
 conn, addr = TCPServerSocket.accept()
 
 print('Client: {}'.format(addr))
 
+# Un po' di attesa per dare tempo al client di connettersi 
 time.sleep(1)
 while True:
+    # ricevo i dati dal client e li salvo in data che è una stringa di byte (1024)
     data = conn.recv(1024)
 
+    # Se non ci sono più dati da ricevere esco dal ciclo ma per fare ciò devo chiudere la connessione e quindi chiudere il terminale del client
     if not data:
         break
 
     #print('{}: echo message: {}'.format(addr, data[:-1].decode('utf-8')))
+    # Stampo l'indirizzo del client e il messaggio che mi ha inviato 
     print('{}: echo message: {}'.format(addr, data))
     
+    # Invio i dati al client 
     conn.sendall(data)
 
-conn.close()
 # Chiudo la connessione in essere
+conn.close()
+
 
 TCPServerSocket.close()
--- socket/tcp_echo_server_multi.py
+++ socket/tcp_echo_server_multi.py
@@ -3,8 +3,10 @@ from sys import argv
 import re
 from threading import Thread, Lock
 
+# questa è la funzione che rappresenta il thread  di gestione della richiesta
 def connection_manager_thread(addr, conn):
     print('Client: {}'.format(addr))
+    # variabile che mi serve per capire se devo inviare o meno i dati al client ed è locale al thread quindi diversa per ogni thread (non è condivisa tra i client)
     toggle = True
     while True:
         data = conn.recv(1024)
@@ -28,10 +30,13 @@ if __name__ == '__main__':
     TCPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
     TCPServerSocket.bind((localIP, localPORT))
 
+    # ogni volta che arriva una richiesta c'è un processo principale che avvia un thread che si occupa di gestire la richiesta, 
+    # ogni richiesta equivale a un thread di gestione avviato
     while True:
         print('TCP Server UP ({},{}), waiting for connections ...'.format(localIP, localPORT))
         TCPServerSocket.listen()
         conn, addr = TCPServerSocket.accept()
+        # creo il thread che deve gestire la connessione e lo avvio 
         Thread(target=connection_manager_thread, args=(addr, conn),).start()
     
     TCPServerSocket.close()
