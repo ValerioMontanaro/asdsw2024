ecc88fc0ecb79fdce4bbf458265e2ffa38a615a2
ValerioMontanaro <valerio.montanaro@alcampus.it>
2024-03-16 16:43:25 +0000

    Commenti chat_server.py

--- socket/chat_server.py
+++ socket/chat_server.py
@@ -3,47 +3,41 @@ from sys import argv
 import re
 from threading import Thread, Lock
 
+#questa funzione viene chiamata dal thread di gestione della connessione
 def sendToAll(addr, data):
     global activeConnections
     global mutex
 
+    # fondamentale l'uso del mutex perche se qualcuno provasse a cambiare la  struttura del  dizionario il ciclo for  andrebbe in errore
     mutex.acquire()
+    # scorro tutte le coppie indirizzo e connessione connesse, e se l'indirizzo non è quello che ha chiesto il servizio lo mando
     for eaddr, econn in activeConnections.items():
         if not eaddr == addr:
             econn.sendall(data)
-    mutex.release()
-
-def sendToUser(daddr, data):
-    global activeConnections
-    global mutex
-
-    mutex.acquire()
-    conn = activeConnections[daddr]:
-    conn.sendall(data)
+    # dopo aver concluso di aver mandato i messaggi a  tutti gli altri allora rilascio il mutex perche il compito è concluso 
     mutex.release()
 
 def connection_manager_thread(addr, conn):
+    # sono puntatori 
     global activeConnections
     global mutex
     print('Client: {}'.format(addr))
+    # non c'è l'opzione di TOGGLE 
     while True:
         data = conn.recv(1024)
         if not data:
             break
         if bool(re.search('^\[STOP\]', data.decode('utf-8'))):
             break
-        if bool(re.search('^\[DM\]', data.decode('utf-8'))):
-            print('immettere logica di invio messaggio privato')
-            # logica di riconoscimento {'addr': '127.0.0.1', 'port': '44444', 'msg': 'messaggio'}
-            # sendToUser(daddr, msg)
-            # continue
         print('{}: chat message: {}'.format(addr, data[:-1].decode('utf-8')))
         
+        
         dataToSend = '{}: {}'.format(addr, data.decode('utf-8'))
-
+        # viene chiamata la funzione di sopra
         sendToAll(addr, dataToSend.encode())
 
     mutex.acquire()
+    # levo dalle connessioni attive perche è chiusa
     del activeConnections[addr]
     mutex.release()
     conn.close()
@@ -52,9 +46,11 @@ if __name__ == '__main__':
 
     localIP     = argv[1]
     localPORT   = int(argv[2])
-
+    
+    # dizionario delle connessione attive
     global activeConnections
     activeConnections = {}
+    # mutex per proteggere il dizionario 
     global mutex
     mutex = Lock()
 
@@ -67,6 +63,7 @@ if __name__ == '__main__':
         while True:
             print('Chat Server UP ({},{}), waiting for connections ...'.format(localIP, localPORT))
             TCPServerSocket.listen()
+            # conn e addr sono puntatori
             conn, addr = TCPServerSocket.accept()
 
             mutex.acquire()
@@ -78,3 +75,4 @@ if __name__ == '__main__':
     finally:
         if TCPServerSocket:
             TCPServerSocket.close()
+            
